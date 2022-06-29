0a9b4ea2de3a48fde43200a6f726ef38b7b2049a
Matheus Oliveira <matheus.oliveira110101@gmail.com>
2022-06-28 21:28:28 -0300

    config routes API and CORS

--- config/VerifyCsrfToken.php
+++ config/VerifyCsrfToken.php
@@ -1,3 +1,4 @@
+
 <?php
 
 namespace App\Http\Middleware;
@@ -12,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '*'
+        //
     ];
 }
